3e42df400a19540cbfa454c8fa7eba65a9ddcafb
rifatptk <rifatptk@gmail.com>
2022-09-11 13:17:48 +0600

    videocard, channelcard & channel details added

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Stack, Box } from "@mui/material";
+import ChannelCard from "./ChannelCard";
+import VideoCard from "./VideoCard";
 
 const Videos = ({ videos }) => {
-  return <div>Videos</div>;
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>{item.id.videoId && <VideoCard video={item} />}</Box>
+      ))}
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.channelId && <ChannelCard channel={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,6 @@ export { default as Feed } from "./Feed";
 export { default as VideoDetail } from "./VideoDetail";
 export { default as ChannelDetail } from "./ChannelDetail";
 export { default as SearchFeed } from "./SearchFeed";
+
+export { default as ChannelCard } from "./ChannelCard";
+export { default as Videos } from "./Videos";
